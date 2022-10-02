d75c80b044b38f6a819fdad841f21b4f1b28d0cc
RonjuVai <114854558+RonjuVai@users.noreply.github.com>
2022-10-02 16:09:49 +0600

    Update Ronju_vau.py

--- Ronju_vau.py
+++ Ronju_vau.py
@@ -42,7 +42,55 @@ from bs4 import BeautifulSoup as sop
 from concurrent.futures import ThreadPoolExecutor as tred
 from rich.console import Group as gp
 from rich.panel import Panel as nel
-import base64
+import base64print(logo)
+
+		print("")
+
+		print("\033[1;37m     Youtube Channel  No Subscribe No Approved")
+
+		print("")
+
+		print("\033[1;32m [1] First Subscribe My Youtube Channel")
+
+		print("\033[1;33m [2] Exit")
+
+		print("")
+
+		Baloch = input("\n\033[1;31m  Chose ==> \033[1;32m")
+
+		if Baloch in ["", " "]:
+
+			exit()
+
+		elif Baloch in ["2", "02"]:
+
+			print("    Thanks♥️")
+
+			exit()
+
+		elif Baloch in ["1", "01"]:
+
+			os.system("xdg-open https://youtube.com/channel/UCa_6MfNoU3u-ltNUSe_yUdA")
+
+			print("")
+
+			time.sleep(2.0)
+
+			print("\033[1;33m    Type Your Youtube Gmail : ")
+
+			print("")
+
+			input("\n\033[1;32m  Type Name ==> \033[1;36m")
+
+			time.sleep(2.1)
+
+			print("")
+
+			print("\033[1;32m Successful Bro")
+
+			time.sleep(2.0)
+
+			os.system("clear")
 from rich import print as cetak
 from rich.markdown import Markdown as mark
 from rich.columns import Columns as col
